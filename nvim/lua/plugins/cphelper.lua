7158afc6d8e711f5585d7c7f1ac0e2eb8a9a2808
azeturk810 <azeturk810nf@gmail.com>
2025-08-10 23:11:03 +0400

    cph delete

--- nvim/lua/plugins/cphelper.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "p00f/cphelper.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    -- require("cphelper.lua").setup()
-  end,
-  lazy = false, -- bu sətri əlavə et!
-}
-
