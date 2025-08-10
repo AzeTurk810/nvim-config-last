88be8b5c6ccb797d2b6dc0543b966946505f2661
azeturk810 <azeturk810nf@gmail.com>
2025-08-10 14:35:54 +0400

    notify is best

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,27 +1,39 @@
 return {
-  'folke/noice.nvim',
-  config = function()
-    require('noice').setup({
-      lsp = {
-        signature = {
-          enabled = false,
-        },
-      },
-      cmdline = {
-        enabled = true,
-        view = 'cmdline_popup'
-      },
-      views = {
-        cmdline_popup = {
-          position = {
-            row = '95%',
-            col = '50%'
-          }
-        }
-      },
-      messages = {
-        enabled = true,
-      },
-    })
-  end
+    'folke/noice.nvim',
+    dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
+    },
+    
+    config = function()
+        require('notify').setup({
+            background_colour = "#000000",
+        });
+        require('noice').setup({
+            lsp = {
+                signature = {
+                    enabled = false,
+                },
+            },
+            cmdline = {
+                enabled = true,
+                view = 'cmdline_popup'
+            },
+            views = {
+                cmdline_popup = {
+                    position = {
+                        row = '95%',
+                        col = '50%'
+                    }
+                }
+            },
+            messages = {
+                enabled = true,
+            },
+        })
+    end
 }
